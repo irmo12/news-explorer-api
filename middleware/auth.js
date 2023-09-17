bc79acd289fd8093d81ecd131ae869c033626668
irmo12 <omriws@hushmail.com>
2023-09-17 10:53:36 +0300

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new Unathorized('Authorization Required'));
+    return next(new Unathorized('Authorization Required'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,10 +16,10 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret');
   } catch (err) {
-    next(new Unathorized('Authorization Required'));
+    return next(new Unathorized('Authorization Required'));
   }
 
   req.user = payload;
 
-  next();
+  return next();
 };
